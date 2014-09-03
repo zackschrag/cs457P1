91e92db9834aee9a75e356829e8b2659e11d8ea3
Zack Schrag <zackschrag@gmail.com>
2014-09-02 20:27:29 -0600

    Setting up socket connection.

--- include/server.h
+++ include/server.h
@@ -2,6 +2,9 @@
 #define SERVER_H
 
 #include <p1.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 class Server {
 public:
--- src/server.cpp
+++ src/server.cpp
@@ -6,4 +6,22 @@ Server::Server() {
 
 void Server::listen(int portNumber) {
 	cout << "listening" << endl;
+	struct sockaddr_in ServAddr;
+	ServAddr.sin_family = AF_INET;
+	ServAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	ServAddr.sin_port = htons(portNumber);
+	int servSock;
+
+	if ((servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+		cout << "servSock: " << servSock << endl;
+	}
+	else {
+		cout << "servSock: " << servSock << endl;
+		if (bind(servSock, (struct sockaddr *) &ServAddr, sizeof(echoServAddr)) < 0) {
+			cout << "bind < 0" << endl;
+		}
+		else {
+			cout << "bind >= 0" << endl;
+		}
+	}
 }
